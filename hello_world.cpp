9070ab06a6a08dcb08fc6336e156090da16bf146
dvorfe30-io <dvorfe30@gmail.com>
2026-03-06 20:38:09 +0300

    Это patch1 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,11 +3,8 @@
 
 int main() {
     std::string name;
-    
     std::cout << "Enter @name: ";
     std::cin >> name;
-    
     std::cout << "Hello world from @" << name << std::endl;
-    
     return 0;
 }
